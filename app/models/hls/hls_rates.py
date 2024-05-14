7c0093b0cbb4f421d929db28a6d381a4fb867be8
ametta <alessandro@ametta.dev>
2024-05-13 23:42:08 -0400

    Added enhanced excel file formatting

--- app/models/hls/hls_rates.py
+++ app/models/hls/hls_rates.py
@@ -1,6 +1,17 @@
 from enum import Enum
+from re import S
 from typing import Optional
 from pydantic import BaseModel
+
+
+pr_codes_mapping = {
+    "COSU": "COSCO",
+    "CMDU": "CMA",
+    "ONEY": "ONE",
+    "HLCU": "HPL",
+    "MEDU": "MSC",
+    "MAEU": "MSK",
+}
 
 
 class HLSRateType(Enum):
@@ -19,8 +30,46 @@ class HLSFilter(BaseModel):
 
 
 class HLSRate(BaseModel):
-    Filter: HLSFilter
+    Charges: list[any]
+    CommodityDetail: str
+    EffectiveDate: str
+    ExpiryDate: str
+    GP20: int
+    GP40: int
+    HQ40: int
+    HQ45: int
+    ID: str
+    NOR40: int
+    PldAMSCode: str
+    PldName: str
+    PlrAMSCode: str
+    PlrName: str
+    PodAMSCode: str
+    PodName: str
+    PodPldTT: int
+    PolAMSCode: str
+    PolName: str
+    PolPodTT: int
+    PolViaTT: int
+    PrCode: str
+    PricingType: str
+    Remark: str
+    SOC20: int
+    SOC40: int
+    SOC40HQ: int
+    SOC45: int
+    SOCNOR40: int
+    ServiceCode: str
+    TruckFee20: int
+    TruckFee40: int
+    ViaAMSCode: str
+    ViaName: str
+    isSOC20: bool
+    isSOC40: bool
+    isSOC40HQ: bool
+    isSOC45: bool
+    isSOCNOR40: bool
 
 
 class HLSRatesRequest(BaseModel):
-    pass
+    Filter: HLSFilter
